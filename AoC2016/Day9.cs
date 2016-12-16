08da056d73e7d3b257954dbbed7eddb8dd7c3f9e
Stuart Harrison <email@stuart-harrison.com>
2016-12-16 09:17:57 +0000

    Day 1 complete and some code changes, seems okay so far.

---
+++ AoC2016/Day9.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AoC2016 {
+    
+    public static class Day9 {
+
+        public static long Part1(char[] input, long n) {
+            long counter = 0;
+            for (int i = 0; i < input.Length; i++) {
+                if (input[i] == ' ') {
+                    continue;
+                }
+                else if (input[i] == '(') {
+                    var marker = new String(input.Skip(i + 1).TakeWhile(x => x != ')').ToArray());
+                    var markerSplit = marker.Split('x');
+
+                    int numberOfCharacters = int.Parse(markerSplit[0]);
+                    int skipper = i + marker.Length + 2;
+
+                    counter += Part1(input.Skip(skipper).Take(numberOfCharacters).ToArray(), int.Parse(markerSplit[1]));
+                    i = skipper + numberOfCharacters - 1;
+                }
+                else {
+                    counter++;
+                }
+            }
+            return counter * n;
+        }
+
+        public static List<char> outputStream = new List<char>();
+
+        public static long DecompressString(string input) {
+            Console.Clear();
+            char[] _input = input.ToCharArray();
+            long counter = 0;
+
+            for (int i = 0; i < _input.Length; i++) {
+                if (_input[i] == ' ') continue;
+
+                if (_input[i] == '(') {
+                    string marker = new String(input.Skip(i + 1).TakeWhile(x => x != ')').ToArray());
+                    string[] aMarker = marker.Split('x');
+
+                    int charSelect = int.Parse(aMarker[0]);
+                    int repeatTimes = int.Parse(aMarker[1]);
+                    
+                    string characters = new String(input.Skip(i + (marker.Length + 2)).Take(charSelect).ToArray());
+                    
+                    
+                    string newCharacters = string.Empty;
+                    for (int c = 0; c < repeatTimes; c++) {
+                        newCharacters += characters;
+                    }
+
+                    //outputStream.AddRange(newCharacters.ToCharArray());
+                    //counter += newCharacters.ToCharArray().Length;
+                    counter += DecompressString(newCharacters);
+                    i = (i + marker.Length + 2) + charSelect - 1;
+                }
+                else {
+                    //outputStream.Add(_input[i]);
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+            return counter;
+        }
+    }
+}
--- DebuggingConsole/Program.cs
+++ DebuggingConsole/Program.cs
@@ -11,8 +11,12 @@ namespace DebuggingConsole {
     class Program {
         
         static void Main(string[] args) {
+            
+            string day1File = DebuggingConsole.Properties.Resources.Day1_Input;
+            //Day1.Run(day1File);
+            Day1.RunPart2(day1File);
 
-            //string _dResource = DebuggingConsole.Properties.Resources.Day8_Input;
+            //string _dResource = DebuggingConsole.Properties.Resources.Day7_Input;
             //_dResource = _dResource.Replace("\r", "");
             //string[] a_dResource = _dResource.Split('\n');
 
@@ -22,12 +26,17 @@ namespace DebuggingConsole {
             //    Day8_2.InputCommand(command);
             //}
 
-            string _dResource = DebuggingConsole.Properties.Resources.Day9_Input;
+            //string _dResource = DebuggingConsole.Properties.Resources.Day9_Input;
             //string day9TestString = "ADVENTA(1x5)BC(3x3)XYZA(2x2)BCD(2x2)EFG(6x1)(1x3)AX(8x2)(3x3)ABCY";
             //string day9TestString = "(27x12)(20x12)(13x14)(7x10)(1x12)A";
 
-            long result = Day9.Part1(_dResource.ToCharArray(), 1);
-            Console.WriteLine(String.Format("Part 1 Solution is... {0}", result));
+            //long result = Day9.Part1(_dResource.ToCharArray(), 1);
+            //Console.WriteLine(String.Format("Part 1 Solution is... {0}", result));
+
+            //string[] day7test = new string[] { "abba[mnop]qrst", "abcd[bddb]xyyx", "aaaa[qwer]tyui", "ioxxoj[asdfgh]zxcvbn" };
+            //int count = Day7.Run(day7test);
+            //int count = Day7.Run(a_dResource);
+            //Console.WriteLine(String.Format("Result: {0}", count));
 
             /// The End is here...
             Console.WriteLine("");
